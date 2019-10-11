9f7e238d78418ed08bdb2259197c42e990840ac1
unknown <anoopdavid@outlook.com>
2019-10-10 20:40:40 -0700

    arm code for outreach

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.DefaultArm;
+
+
+/**
+ * Add your docs here.
+ */
+public class Arm extends Subsystem {
+  public WPI_TalonSRX catapult = new WPI_TalonSRX(RobotMap.armport);
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+     setDefaultCommand(new DefaultArm());
+  }
+}
